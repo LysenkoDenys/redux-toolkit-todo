a52a57f90aeacddd0121a86d5bb20fd54d0a2958
Lysenko <lysenkoden@gmail.com>
2024-07-01 21:10:11 +0200

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState('');
+
+  return <div className="text-center text-2xl">Hello!</div>;
 }
 
 export default App;
